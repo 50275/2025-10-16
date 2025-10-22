10abb40b787f7b87e92d6b8a1c71d3c782259053
50275 <68676339+50275@users.noreply.github.com>
2025-10-22 13:35:26 -0700

    Update sculptureScript.js

--- sculptureScript.js
+++ sculptureScript.js
@@ -1,1 +1,5 @@
-
+const sculptureList = require('./data.js'); // import data.js
+let out = sculptureList.map(i => { 
+    return Object.entries(i).map(([key, val]) => ({key: key, length: val.length})); 
+});
+console.log(out);
