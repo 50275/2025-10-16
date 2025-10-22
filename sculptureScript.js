f53bf1db82288210a75e68937406a9f38e021f3c
50275 <68676339+50275@users.noreply.github.com>
2025-10-22 13:38:21 -0700

    flex

--- sculptureScript.js
+++ sculptureScript.js
@@ -3,3 +3,5 @@ let out = sculptureList.map(i => {
     return Object.entries(i).map(([key, val]) => ({key: key, length: val.length})); 
 });
 console.log(out);
+
+// console.log(require('./data.js').map(i => { return Object.entries(i).map(([key, val]) => ({key: key, length: val.length}));}));
