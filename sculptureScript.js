8ddd0e6169333b5b80e2557b99409f08f4e20a85
50275 <68676339+50275@users.noreply.github.com>
2025-10-22 13:45:23 -0700

    sculptureListLengths moment

--- sculptureScript.js
+++ sculptureScript.js
@@ -1,7 +1,5 @@
 const sculptureList = require('./data.js'); // import data.js
-let out = sculptureList.map(i => { 
+let sculptureListLengths = sculptureList.map(i => { 
     return Object.entries(i).map(([key, val]) => ({key: key, length: val.length})); 
 });
-console.log(out);
-
-// console.log(require('./data.js').map(i => { return Object.entries(i).map(([key, val]) => ({key: key, length: val.length}));}));
+// console.log(sculptureListLengths);
